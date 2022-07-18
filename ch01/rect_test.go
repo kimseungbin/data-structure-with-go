46ae9fc1b6ef7581588a89f06a451b7bc691d5fa
SeungBin Kim <seungbin0508@gmail.com>
2022-07-18 15:40:06 +0900

    Add test example for Rect

--- ch01/rect_test.go
+++ ch01/rect_test.go
@@ -1,6 +1,9 @@
 package ch01
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRect_Area(t *testing.T) {
 	r := Rect{
@@ -14,4 +17,14 @@ func TestRect_Area(t *testing.T) {
 	if got != want {
 		t.Errorf("got %f want %f", got, want)
 	}
+}
+
+func ExampleRect_Area() {
+	r := Rect{
+		width:  10,
+		height: 10,
+	}
+	area := r.Area()
+	fmt.Println(area)
+	// Output: 100
 }
