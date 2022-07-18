3f3847d8851fb85616f5fe56a68c15cfd1b63965
SeungBin Kim <seungbin0508@gmail.com>
2022-07-18 15:41:33 +0900

    Add perimeter test for Rect

--- ch01/rect_test.go
+++ ch01/rect_test.go
@@ -28,3 +28,17 @@ func ExampleRect_Area() {
 	fmt.Println(area)
 	// Output: 100
 }
+
+func TestRect_Perimeter(t *testing.T) {
+	r := Rect{
+		width:  5,
+		height: 10,
+	}
+
+	got := r.Perimeter()
+	want := 30.0
+
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
