aa85a8d358c4240298da5e2570b24862c8a49bec
SeungBin Kim <seungbin0508@gmail.com>
2022-07-21 16:45:20 +0900

    Implement binarySearchRecursive function

--- ch01/binarySearchRecursive.go
+++ ch01/binarySearchRecursive.go
@@ -1,5 +1,12 @@
 package ch01
 
 func BinarySearchRecursive(data []int, low, high, value int) int {
-	return 0
+	mid := low + (high-low)/2
+	if data[mid] == value {
+		return mid
+	} else if data[mid] < value {
+		return BinarySearchRecursive(data, mid+1, high, value)
+	} else {
+		return BinarySearchRecursive(data, low, mid-1, value)
+	}
 }
