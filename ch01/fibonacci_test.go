a7426512e4763ea498a311e824f263d3ff645616
SeungBin Kim <seungbin0508@gmail.com>
2022-07-21 16:08:44 +0900

    Add example for Fibonacci function

--- ch01/fibonacci_test.go
+++ ch01/fibonacci_test.go
@@ -1,6 +1,9 @@
 package ch01
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestFibonacci(t *testing.T) {
 	got := Fibonacci(10)
@@ -9,4 +12,10 @@ func TestFibonacci(t *testing.T) {
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
+}
+
+func ExampleFibonacci() {
+	fibonacci := Fibonacci(10)
+	fmt.Println(fibonacci)
+	// Output: 55
 }
